6624272b727f20f880ed7b10a962986b0faf226f
liuweikai <vicliu@outlook.com>
2019-09-23 13:30:32 +0800

    移除Hystrix

--- gateway/src/main/java/indi/vicliu/juaner/gateway/GatewayApplication.java
+++ gateway/src/main/java/indi/vicliu/juaner/gateway/GatewayApplication.java
@@ -2,7 +2,6 @@ package indi.vicliu.juaner.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -14,7 +13,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
-@EnableCircuitBreaker
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
