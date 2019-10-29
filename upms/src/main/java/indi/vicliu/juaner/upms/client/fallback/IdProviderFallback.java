9116c8fd52be06be4cc919a846955f172c21dcab
liuweikai <vicliu@outlook.com>
2019-10-29 15:25:37 +0800

    使用lombok

--- upms/src/main/java/indi/vicliu/juaner/upms/client/fallback/IdProviderFallback.java
+++ upms/src/main/java/indi/vicliu/juaner/upms/client/fallback/IdProviderFallback.java
@@ -1,18 +1,18 @@
 package indi.vicliu.juaner.upms.client.fallback;
 
 import indi.vicliu.juaner.upms.client.IdProvider;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class IdProviderFallback implements IdProvider {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(IdProviderFallback.class);
-
     @Override
     public long nextId() {
-        LOGGER.error("genId的熔断被触发");
+        log.error("genId的熔断被触发");
         return -1;
     }
 }
