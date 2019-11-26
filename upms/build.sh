cb62a2edcc95c53398f3f1f2cc7848f921ffdbc9
zhujia <magicletters@qq.com>
2019-11-26 13:24:57 +0800

    build docker

---
+++ upms/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd `dirname "$0"`
+mvn -Dmaven.test.skip=true clean package
+docker build -t registry.cn-chengdu.aliyuncs.com/magicletters/upms:latest .
+docker push registry.cn-chengdu.aliyuncs.com/magicletters/upms:latest
