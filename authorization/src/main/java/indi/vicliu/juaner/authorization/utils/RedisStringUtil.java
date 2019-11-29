d67e7b802422051428050510370459cd3e1afe7f
liuweikai <vicliu@outlook.com>
2019-11-29 20:22:33 +0800

    删除了用不到的方法

--- authorization/src/main/java/indi/vicliu/juaner/authorization/utils/RedisStringUtil.java
+++ authorization/src/main/java/indi/vicliu/juaner/authorization/utils/RedisStringUtil.java
@@ -5,10 +5,6 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
-
 @Component
 public class RedisStringUtil {
 
@@ -19,31 +15,4 @@ public class RedisStringUtil {
         ValueOperations<String, String> ops = template.opsForValue();
         ops.set(key,value);
     }
-    
-    public void setKeyExpire(String key, String value,int time,TimeUnit unit) {
-		ValueOperations<String, String> ops = template.opsForValue();
-		ops.set(key, value,time,unit);
-	}
-
-    public String getValue(String key){
-        ValueOperations<String, String> ops = template.opsForValue();
-        return ops.get(key);
-    }
-
-    public Long incr(String key){
-        ValueOperations<String, String> ops = template.opsForValue();
-        return ops.increment(key);
-    }
-
-    public Set<String> searchKeyLike(String key){
-        return template.keys(key);
-    }
-
-    public void delKey(String key){
-        template.delete(key);
-    }
-    public void delKeyList(List<String> key){
-        template.delete(key);
-    }
-
 }
