08ad373371019a7ea9dd7ff3eb23eac1ae3d7b70
liuweikai <vicliu@outlook.com>
2019-11-29 18:21:48 +0800

    在redis中记录jti用户做单点登录

--- authorization/src/main/java/indi/vicliu/juaner/authorization/aspect/AuthTokenAspect.java
+++ authorization/src/main/java/indi/vicliu/juaner/authorization/aspect/AuthTokenAspect.java
@@ -1,13 +1,25 @@
 package indi.vicliu.juaner.authorization.aspect;
 
+import com.alibaba.fastjson.JSONObject;
+import indi.vicliu.juaner.authorization.utils.RedisStringUtil;
+import indi.vicliu.juaner.common.core.CommonConstant;
 import indi.vicliu.juaner.common.core.exception.ErrorType;
 import indi.vicliu.juaner.common.core.message.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.InvalidClientException;
+import org.springframework.security.oauth2.common.exceptions.InvalidGrantException;
+import org.springframework.security.oauth2.common.exceptions.InvalidRequestException;
+import org.springframework.security.oauth2.common.exceptions.UnsupportedGrantTypeException;
 import org.springframework.stereotype.Component;
 
 /**
@@ -19,14 +31,50 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Slf4j
 public class AuthTokenAspect {
-    @Around("execution(* org.springframework.security.oauth2.provider.endpoint.TokenEndpoint.postAccessToken(..))")
+
+    @Autowired
+    private RedisStringUtil redisStringUtil;
+
+    @Pointcut("execution(* org.springframework.security.oauth2.provider.endpoint.TokenEndpoint.postAccessToken(..))")
+    public void postAccessTokenPointcut() {
+    }
+
+    @AfterThrowing(pointcut = "postAccessTokenPointcut()", throwing = "e")
+    public Result afterThrowing(JoinPoint joinPoint, Throwable e) {
+        log.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL");
+        if(e instanceof InsufficientAuthenticationException){
+            return Result.fail("没有clientId身份验证");
+        } else if(e instanceof InvalidClientException){
+            return Result.fail("不可用的clientId");
+        } else if(e instanceof InvalidRequestException){
+            return Result.fail("缺少grant type");
+        } else if(e instanceof InvalidGrantException){
+            return Result.fail("不支持隐式授权");
+        } else if(e instanceof UnsupportedGrantTypeException){
+            return Result.fail("不支持这样的grant type");
+        } else {
+            return Result.fail("授权出错");
+        }
+    }
+
+    //@Around("execution(* org.springframework.security.oauth2.provider.endpoint.TokenEndpoint.postAccessToken(..))")
+    @Around("postAccessTokenPointcut()")
     public Object handleControllerMethod(ProceedingJoinPoint pjp) throws Throwable {
         Result result = null;
         Object proceed = pjp.proceed();
         if (proceed != null) {
             ResponseEntity<OAuth2AccessToken> responseEntity = (ResponseEntity<OAuth2AccessToken>)proceed;
-            OAuth2AccessToken body = responseEntity.getBody();
             if (responseEntity.getStatusCode().is2xxSuccessful()){
+                OAuth2AccessToken body = responseEntity.getBody();
+                String strAccessToken = JSONObject.toJSONString(body);
+                log.debug("strAccessToken:{}",strAccessToken);
+                if(body == null){
+                    result = Result.fail("token出错");
+                    return ResponseEntity.status(200).body(result);
+                }
+                log.debug("user jti:{}",redisStringUtil.getValue(CommonConstant.USER_TOKEN_KEY + body.getAdditionalInformation().get("user")));
+                redisStringUtil.setKey(CommonConstant.USER_TOKEN_KEY + body.getAdditionalInformation().get("user"),(String)body.getAdditionalInformation().get("jti"));
                 result = Result.success(body);
             } else {
                 log.error("error:{}", responseEntity.getStatusCode().toString());
---
+++ authorization/src/main/java/indi/vicliu/juaner/authorization/utils/RedisStringUtil.java
@@ -0,0 +1,49 @@
+package indi.vicliu.juaner.authorization.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RedisStringUtil {
+
+    @Autowired
+    private StringRedisTemplate template;
+
+    public void setKey(String key,String value){
+        ValueOperations<String, String> ops = template.opsForValue();
+        ops.set(key,value);
+    }
+    
+    public void setKeyExpire(String key, String value,int time,TimeUnit unit) {
+		ValueOperations<String, String> ops = template.opsForValue();
+		ops.set(key, value,time,unit);
+	}
+
+    public String getValue(String key){
+        ValueOperations<String, String> ops = template.opsForValue();
+        return ops.get(key);
+    }
+
+    public Long incr(String key){
+        ValueOperations<String, String> ops = template.opsForValue();
+        return ops.increment(key);
+    }
+
+    public Set<String> searchKeyLike(String key){
+        return template.keys(key);
+    }
+
+    public void delKey(String key){
+        template.delete(key);
+    }
+    public void delKeyList(List<String> key){
+        template.delete(key);
+    }
+
+}
