1c5442de40cbf426e79f22807cfc7aef6b40b470
lwq523698741 <lwq523698741@163.com>
2019-12-24 16:36:47 +0800

    添加支持WebSocket

--- gateway/src/main/java/indi/vicliu/juaner/gateway/filter/WebSockerFilter.java
+++ gateway/src/main/java/indi/vicliu/juaner/gateway/filter/WebSockerFilter.java
@@ -5,74 +5,65 @@ import indi.vicliu.juaner.common.core.CommonConstant;
 import indi.vicliu.juaner.gateway.client.service.AuthService;
 import indi.vicliu.juaner.gateway.utils.RedisStringUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.filter.WebsocketRoutingFilter;
+import org.springframework.cloud.gateway.filter.headers.HttpHeadersFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.jwt.Jwt;
+import org.springframework.web.reactive.socket.client.WebSocketClient;
+import org.springframework.web.reactive.socket.server.WebSocketService;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 /**
- * @Auther: liuweikai
- * @Date: 2019-03-22 12:56
- * @Description: 认证过滤器
- */
+ * @program: juaner-platform
+ * @description:
+ * @author: liuwenqi
+ * @create: 2019-12-23 18:12
+ **/
 @Slf4j
 @Configuration
-public class AccessGatewayFilter implements GlobalFilter {
+public class WebSockerFilter extends WebsocketRoutingFilter {
+    public WebSockerFilter(WebSocketClient webSocketClient, WebSocketService webSocketService, ObjectProvider<List<HttpHeadersFilter>> headersFiltersProvider) {
+        super(webSocketClient, webSocketService, headersFiltersProvider);
+    }
 
-    /**
-     * 由authentication-client模块提供签权的feign客户端
-     */
     @Autowired
     private AuthService authService;
 
     @Autowired
     private RedisStringUtil redisStringUtil;
 
-    @Autowired
-    private WebSockerFilter webSockerFilter;
 
-    private static String WEBSOCKET_PROTOCOL = "Sec-WebSocket-Protocol";
-    /**
-     * 1.首先网关检查token是否有效，无效直接返回401，不调用签权服务
-     * 2.调用签权服务器看是否对该请求有权限，有权限进入下一个filter，没有权限返回401
-     *
-     * @param exchange
-     * @param chain
-     * @return
-     */
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
-        String authentication = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        final List<String> strings = request.getHeaders().get("Sec-WebSocket-Protocol");
+
+        String authentication = null;
+        if (strings != null && strings.size() > 0) {
+            authentication = strings.get(0);
+        }
+        log.info("websoket 传入的 Token {}" ,authentication);
+
+        if (authentication == null) { //如果token为空则直接报权限异常
+            return unauthorized(exchange);
+        }else {
+            authentication = "bearer " + authentication;
+        }
         String method = request.getMethodValue();
         String url = request.getPath().value();
         log.debug("url:{},method:{},headers:{}", url, method, request.getHeaders());
-        if (StringUtils.isNotEmpty(request.getHeaders().getFirst(WEBSOCKET_PROTOCOL))) {
-            log.info("WebSocket In AccessGatewayFilter ");
-            return webSockerFilter.filter(exchange,chain);
-        }
-        //不需要网关签权的url
-        if (authService.ignoreAuthentication(url)) {
-            return chain.filter(exchange);
-        }
-
-        if(StringUtils.isEmpty(authentication)){
-            return unauthorized(exchange);
-        }
-
         Jwt jwt = authService.getJwt(authentication);
-
         //校验jwt
         //token是否有效
         if (authService.invalidJwtAccessToken(jwt)) {
@@ -97,8 +88,9 @@ public class AccessGatewayFilter implements GlobalFilter {
             ServerHttpRequest.Builder builder = request.mutate();
             //TODO 转发的请求都加上服务间认证token
             builder.header(CommonConstant.X_CLIENT_TOKEN_USER, claims);
-            return chain.filter(exchange.mutate().request(builder.build()).build());
+            return super.filter(exchange.mutate().request(builder.build()).build(),chain);
         }
+
         return unauthorized(exchange);
     }
 
@@ -113,6 +105,7 @@ public class AccessGatewayFilter implements GlobalFilter {
                 .bufferFactory().wrap(HttpStatus.UNAUTHORIZED.getReasonPhrase().getBytes(StandardCharsets.UTF_8));
         return serverWebExchange.getResponse().writeWith(Flux.just(buffer));
     }
+
 
     /**
      *
