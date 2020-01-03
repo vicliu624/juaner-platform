977e1f9d5c53fd56202f3e40cf9d23ad3b7b85c2
lwq523698741 <lwq523698741@163.com>
2020-01-03 18:18:35 +0800

    upms添加用户取消验证电话号码

--- upms/src/main/java/indi/vicliu/juaner/upms/domain/service/impl/UserServiceImpl.java
+++ upms/src/main/java/indi/vicliu/juaner/upms/domain/service/impl/UserServiceImpl.java
@@ -73,13 +73,13 @@ public class UserServiceImpl implements UserService {
     @Override
     public Result addUserInfo(AddUserInfoVO userInfo) throws UserException {
 
-        Example example = new Example(TblUserInfo.class);
+ /*       Example example = new Example(TblUserInfo.class);
         example.createCriteria().andEqualTo("phone",userInfo.getPhone());
         example.setOrderByClause(" create_time desc limit 1");
         List<TblUserInfo> userInfoList = this.userInfoMapper.selectByExample(example);
         if(userInfoList.size()>0){
-            return Result.fail("用户已存在");
-        }
+            return Result.success("用户已存在");
+        }*/
         //保存用户信息
         TblUserInfo info=new TblUserInfo();
         BeanUtils.copyProperties(userInfo,info);
