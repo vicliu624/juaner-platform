0b205d68586707637350b8049ede91a77b6d4a8f
liuweikai <vicliu@outlook.com>
2020-07-15 11:42:47 +0800

    封装SpringBootAdmin

--- admin/src/main/java/indi/vicliu/juaner/admin/controller/SpringBootAdminController.java
+++ admin/src/main/java/indi/vicliu/juaner/admin/controller/SpringBootAdminController.java
@@ -1,7 +1,9 @@
 package indi.vicliu.juaner.admin.controller;
 
 import de.codecentric.boot.admin.server.domain.entities.Application;
+import de.codecentric.boot.admin.server.domain.entities.Instance;
 import de.codecentric.boot.admin.server.services.ApplicationRegistry;
+import de.codecentric.boot.admin.server.services.InstanceRegistry;
 import indi.vicliu.juaner.common.core.message.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
@@ -22,25 +24,24 @@ import java.util.Map;
 public class SpringBootAdminController {
 
     @Autowired
-    private ApplicationRegistry registry;
+    private ApplicationRegistry applicationRegistry;
+
+    @Autowired
+    private InstanceRegistry instanceRegistry;
 
     @GetMapping(value = "/applications",produces = MediaType.APPLICATION_JSON_VALUE)
     public Result applications(){
         List<Application> apps = new ArrayList<>();
-        Flux<Application> appsFlux = registry.getApplications();
+        Flux<Application> appsFlux = applicationRegistry.getApplications();
         appsFlux.subscribe(apps::add);
         log.info("size:{}",apps.size());
         return Result.success(apps);
     }
 
-    @PostMapping(value = "/application")
-    public Result application(@RequestBody Map<String,String> application){
+    @GetMapping(value = "/application")
+    public Result application(@RequestParam("name") String name){
         List<Application> apps = new ArrayList<>();
-        if(application == null || StringUtils.isEmpty(application.get("name"))){
-            return Result.fail("name == null");
-        }
-        String name = application.get("name");
-        Mono<Application> appMono = registry.getApplication(name);
+        Mono<Application> appMono = applicationRegistry.getApplication(name);
         appMono.subscribe(apps::add);
         log.info("size:{}",apps.size());
         return apps.isEmpty() ? Result.fail("未查询到这样的application") : Result.success(apps.get(0));
@@ -54,12 +55,39 @@ public class SpringBootAdminController {
         String name = application.get("name");
         log.debug("Unregister application with name '{}'", name);
         try{
-            registry.deregister(name).collectList().map((deregistered) -> !deregistered.isEmpty()
+            applicationRegistry.deregister(name).collectList().map((deregistered) -> !deregistered.isEmpty()
                     ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build());
             return Result.success("删除成功");
         } catch (Exception e){
             log.error("删除application失败",e);
             return Result.fail("删除失败"+ e.getMessage());
         }
+    }
+
+    @GetMapping(path = "/instances")
+    public Result instances(){
+        List<Instance> instances = new ArrayList<>();
+        Flux<Instance> instanceFlux = instanceRegistry.getInstances().filter(Instance::isRegistered);
+        instanceFlux.subscribe(instances::add);
+        log.info("size:{}",instances.size());
+        return Result.success(instances);
+    }
+
+    @GetMapping(path = "/instances")
+    public Result instances(@RequestParam("name") String name) {
+        List<Instance> instances = new ArrayList<>();
+        Flux<Instance> instanceFlux = instanceRegistry.getInstances(name).filter(Instance::isRegistered);
+        instanceFlux.subscribe(instances::add);
+        log.info("size:{}",instances.size());
+        return Result.success(instances);
+    }
+
+    @GetMapping(path = "/instance")
+    public Result instant(@RequestParam("id") String id) {
+        List<Instance> instances = new ArrayList<>();
+        Flux<Instance> instanceFlux = instanceRegistry.getInstances(id).filter(Instance::isRegistered);
+        instanceFlux.subscribe(instances::add);
+        log.info("size:{}",instances.size());
+        return instances.isEmpty() ? Result.fail("未查询到这样的instance") : Result.success(instances.get(0));
     }
 }
