5bb5b6df76d67b1f7c0e75ee4107549dcaa4eb1f
liuweikai <vicliu@outlook.com>
2020-08-13 14:29:28 +0800

    fixed:bug

--- upms/src/main/java/indi/vicliu/juaner/upms/controller/PermissionController.java
+++ upms/src/main/java/indi/vicliu/juaner/upms/controller/PermissionController.java
@@ -26,114 +26,114 @@ public class PermissionController {
     @Autowired
     private PermissionService permissionService;
 
-
-
     @GetMapping("/all")
-    public Result getAll(){
-        try{
+    public Result getAll() {
+        try {
             return Result.success(permissionService.findAll());
-        } catch (Exception e){
-            log.error(" getAll fail ",e);
+        } catch (Exception e) {
+            log.error(" getAll fail ", e);
             return Result.fail("查询权限时出错");
         }
     }
 
     @GetMapping("/roles")
-    public Result queryByRoles(@RequestParam List<String> roles){
-        try{
-            if(roles == null || roles.size() == 0){
+    public Result queryByRoles(@RequestParam List<String> roles) {
+        try {
+            if (roles == null || roles.size() == 0) {
                 return Result.fail("无角色可查");
             }
 
             String[] strings = new String[roles.size()];
             return Result.success(permissionService.queryByRoles(roles.toArray(strings)));
-        } catch (Exception e){
-            log.error(" queryByRoles fail ",e);
+        } catch (Exception e) {
+            log.error(" queryByRoles fail ", e);
             return Result.fail("查询权限时出错");
         }
     }
 
     @GetMapping("/uri")
-    public Result queryByUri(@RequestParam String uri,@RequestParam String method){
-        try{
-            return Result.success(permissionService.findByURI(uri,method));
-        } catch (Exception e){
-            log.error(" queryByUri fail ",e);
+    public Result queryByUri(@RequestParam String uri, @RequestParam String method) {
+        try {
+            return Result.success(permissionService.findByURI(uri, method));
+        } catch (Exception e) {
+            log.error(" queryByUri fail ", e);
             return Result.fail("查询权限时出错");
         }
     }
 
     /**
      * 获取权限列表
+     *
      * @param jsonMap
      * @return
      */
     @PostMapping("/list")
-    public Result list(@RequestBody Map<String,Object> jsonMap){
+    public Result list(@RequestBody Map<String, Object> jsonMap) {
         Integer pageNum = jsonMap.get("pageNum") == null ? 1 : (Integer) jsonMap.get("pageNum");
         Integer pageSize = jsonMap.get("pageSize") == null ? 10 : (Integer) jsonMap.get("pageSize");
         Boolean isExp = jsonMap.get("isExp") == null ? false : (Boolean) jsonMap.get("isExp");
-        try{
-            List<TblPermissionInfo> list = this.permissionService.list(jsonMap);
-            if(!isExp) {
+        try {
+            if (!isExp) {
                 PageHelper.startPage(pageNum, pageSize);
-                PageInfo<TblPermissionInfo> pageInfo = new PageInfo<TblPermissionInfo>(list);
-                return Result.success(pageInfo);
-            }else{
-                return Result.success(list);
             }
-        }catch (Exception e){
-            log.error("调用获取权限列表的方法list出错",e);
+            List<TblPermissionInfo> list = this.permissionService.list(jsonMap);
+            PageInfo<TblPermissionInfo> pageInfo = new PageInfo<TblPermissionInfo>(list);
+            return Result.success(pageInfo);
+        } catch (Exception e) {
+            log.error("调用获取权限列表的方法list出错", e);
             return Result.fail("查询权限失败");
         }
     }
 
     /**
      * 保存权限信息
+     *
      * @param jsonMap
      * @return
      */
     @PostMapping("/savePermission")
-    public Result savePermission(@RequestBody Map<String,Object> jsonMap){
+    public Result savePermission(@RequestBody Map<String, Object> jsonMap) {
         try {
             return Result.success(permissionService.savePermission(jsonMap));
-        }catch (Exception e){
-            log.info("保存权限信息异常",e);
+        } catch (Exception e) {
+            log.info("保存权限信息异常", e);
             return Result.fail("保存权限信息失败");
         }
     }
 
     /**
      * 删除权限信息
+     *
      * @param jsonMap
      * @return
      */
     @PostMapping("/removePermission")
-    public Result removePermission(@RequestBody Map<String,Object> jsonMap){
+    public Result removePermission(@RequestBody Map<String, Object> jsonMap) {
         String ids = jsonMap.get("ids") == null ? null : jsonMap.get("ids").toString();
-        if(StringUtils.isEmpty(ids)){
+        if (StringUtils.isEmpty(ids)) {
             return Result.fail("删除权限失败");
         }
-        try{
+        try {
             permissionService.removePermission(ids);
             return Result.success("删权限成功");
-        }catch (Exception e){
-            log.error("删除权限方法removePermission出错",e);
+        } catch (Exception e) {
+            log.error("删除权限方法removePermission出错", e);
             return Result.fail("删除权限失败");
         }
     }
 
     /**
      * 获取用户的权限列表
+     *
      * @param id
      * @return
      */
     @GetMapping("/getUserPermission")
     public Result getUserPermission(@RequestParam("id") Long id) {
-        try{
+        try {
             return Result.success(permissionService.getUserPermission(id));
-        } catch (Exception e){
-            log.error("查询权限出错",e);
+        } catch (Exception e) {
+            log.error("查询权限出错", e);
             return Result.fail("查询权限失败");
         }
 
--- upms/src/main/java/indi/vicliu/juaner/upms/controller/RoleController.java
+++ upms/src/main/java/indi/vicliu/juaner/upms/controller/RoleController.java
@@ -31,13 +31,13 @@ public class RoleController {
     private RoleService roleService;
 
     @GetMapping("/roles")
-    public Result getFullUserInfoByUserName(@RequestParam Long userId){
-        try{
+    public Result getFullUserInfoByUserName(@RequestParam Long userId) {
+        try {
             List<TblRoleInfo> roleInfoList = roleService.queryRolesByUserId(userId);
             return Result.success(roleInfoList);
-        } catch (Exception e){
-            log.error(" getFullUserInfoByUserName fail ",e);
-            if(e instanceof RoleException){
+        } catch (Exception e) {
+            log.error(" getFullUserInfoByUserName fail ", e);
+            if (e instanceof RoleException) {
                 return Result.fail(e.getMessage());
             } else {
                 return Result.fail("查询用的角色时出错");
@@ -47,64 +47,69 @@ public class RoleController {
 
     /**
      * 根据角色名获取角色信息
+     *
      * @param roleName
      * @return
      */
     @GetMapping("/getRoleByName")
-    public Result getRoleByName(@RequestParam String roleName){
-        try{
+    public Result getRoleByName(@RequestParam String roleName) {
+        try {
             return Result.success(roleService.getRoleByName(roleName));
-        } catch (Exception e){
+        } catch (Exception e) {
             return Result.fail("查询出错:" + e.getMessage());
         }
     }
 
     /**
      * 添加角色
+     *
      * @param role
      * @return
      */
     @PostMapping("/addRole")
-    public Result addRole(@RequestBody TblRoleInfo role){
-        try{
+    public Result addRole(@RequestBody TblRoleInfo role) {
+        try {
             roleService.addRole(role);
             return Result.success();
-        } catch (Exception e){
+        } catch (Exception e) {
             return Result.fail("创建失败:" + e.getMessage());
         }
     }
 
     /**
      * 修改角色信息
+     *
      * @param role
      * @return
      */
     @PutMapping("/updateRole")
-    public Result updateRole(@RequestBody TblRoleInfo role){
-        try{
+    public Result updateRole(@RequestBody TblRoleInfo role) {
+        try {
             roleService.updateRole(role);
             return Result.success();
-        } catch (Exception e){
+        } catch (Exception e) {
             return Result.fail("更新失败:" + e.getMessage());
         }
     }
 
     /**
      * 管理端获取用户角色
+     *
      * @param jsonMap
      * @return
      */
     @PostMapping("/findRoleByUser")
-    public Result findRoleByUser(@RequestBody Map<String, Object> jsonMap){
-        try{
+    public Result findRoleByUser(@RequestBody Map<String, Object> jsonMap) {
+        try {
             return Result.success(roleService.findRoleByUser(jsonMap));
-        } catch (Exception e){
+        } catch (Exception e) {
             return Result.fail("查询角色时出错");
         }
     }
 
     /**
      * 管理端保存用户信息的角色
+     *
      * @param jsonMap
      * @return
      */
@@ -120,94 +125,97 @@ public class RoleController {
     }
 
     /**
-     *获取角色列表
+     * 获取角色列表
+     *
      * @param jsonMap
      * @return
      */
     @PostMapping("/getRoleInfoList")
-    public Result getRoleInfoList(@RequestBody Map<String,Object> jsonMap){
-        log.info("sysManage getRoleInfoList:{}",jsonMap.toString());
+    public Result getRoleInfoList(@RequestBody Map<String, Object> jsonMap) {
+        log.info("sysManage getRoleInfoList:{}", jsonMap.toString());
         Integer pageNum = jsonMap.get("pageNum") == null ? 1 : (Integer) jsonMap.get("pageNum");
         Integer pageSize = jsonMap.get("pageSize") == null ? 10 : (Integer) jsonMap.get("pageSize");
         Boolean isExp = jsonMap.get("isExp") == null ? false : (Boolean) jsonMap.get("isExp");
-        List<TblRoleInfo> list = roleService.getRoleInfoList(jsonMap);
-        try{
-            if(!isExp) {
-                PageHelper.startPage(pageNum, pageSize);
-                PageInfo<TblRoleInfo> pageInfo = new PageInfo<>(list);
-                return Result.success(pageInfo);
-            }else{
-                return Result.success(list);
-            }
-        }catch (Exception e){
-            log.error("调用获取角色列表的方法getRoleInfoList出错",e);
+        if (!isExp) {
+            PageHelper.startPage(pageNum, pageSize);
+        }
+        try {
+            List<TblRoleInfo> list = roleService.getRoleInfoList(jsonMap);
+            PageInfo<TblRoleInfo> pageInfo = new PageInfo<>(list);
+            return Result.success(pageInfo);
+        } catch (Exception e) {
+            log.error("调用获取角色列表的方法getRoleInfoList出错", e);
             return Result.fail("查询角色失败");
         }
     }
 
     /**
-     *保存角色信息
+     * 保存角色信息
+     *
      * @param jsonMap
      * @return
      */
     @PostMapping("/saveRoleInfo")
-    public Result saveRoleInfo(@RequestBody Map<String,Object> jsonMap){
+    public Result saveRoleInfo(@RequestBody Map<String, Object> jsonMap) {
         try {
             JSONObject jsonObject = roleService.saveRoleInfo(jsonMap);
             return Result.success(jsonObject);
-        }catch (Exception e){
-            log.info("保存角色信息异常",e);
+        } catch (Exception e) {
+            log.info("保存角色信息异常", e);
             return Result.fail("保存角色信息失败");
         }
     }
 
     /**
-     *删除角色信息
+     * 删除角色信息
+     *
      * @param jsonMap
      * @return
      */
     @PostMapping("/removeRoleInfo")
-    public Result removeRoleInfo(@RequestBody Map<String,Object> jsonMap){
+    public Result removeRoleInfo(@RequestBody Map<String, Object> jsonMap) {
         String ids = jsonMap.get("ids") == null ? null : jsonMap.get("ids").toString();
-        if(StringUtils.isEmpty(ids)){
+        if (StringUtils.isEmpty(ids)) {
             return Result.fail("删除权限失败");
         }
-        try{
+        try {
             roleService.removeRoleInfo(ids);
             return Result.success("删除角色成功");
-        }catch (Exception e){
-            log.error("删除角色方法removeRoleInfo出错",e);
+        } catch (Exception e) {
+            log.error("删除角色方法removeRoleInfo出错", e);
             return Result.fail("删除角色失败");
         }
     }
 
     /**
-     *查找角色的权限以及所有权限集合
+     * 查找角色的权限以及所有权限集合
+     *
      * @param jsonMap
      * @return
      */
     @PostMapping("/findPermissionByRole")
-    public Result findPermissionByRole(@RequestBody Map<String,Object> jsonMap){
-        try{
+    public Result findPermissionByRole(@RequestBody Map<String, Object> jsonMap) {
+        try {
             return Result.success(roleService.findPermissionByRole(jsonMap));
-        } catch (Exception e){
-            log.error("查u想你权限出错",e);
+        } catch (Exception e) {
+            log.error("查u想你权限出错", e);
             return Result.fail(e.getMessage());
         }
     }
 
     /**
-     *分配角色的权限
+     * 分配角色的权限
+     *
      * @param jsonMap
      * @return
      */
     @PostMapping("/saveRolePerm")
-    public Result saveRolePerm(@RequestBody Map<String,Object> jsonMap){
-        try{
+    public Result saveRolePerm(@RequestBody Map<String, Object> jsonMap) {
+        try {
             roleService.saveRolePerm(jsonMap);
             return Result.success("角色权限分配成功");
-        }catch (Exception e){
-            log.error("分配角色的权限方法saveRolePerm出错",e);
+        } catch (Exception e) {
+            log.error("分配角色的权限方法saveRolePerm出错", e);
             return Result.fail("分配角色权限失败");
         }
     }
