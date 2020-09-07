c8c8f51c66683d5cd779a33416537c89ee159254
vicliu <32317497+vicliu624@users.noreply.github.com>
2020-09-07 10:42:19 +0800

    加坐标解析

---
+++ common/src/main/java/indi/vicliu/juaner/common/data/extension/mybatis/VirtualGenerated.java
@@ -0,0 +1,11 @@
+package indi.vicliu.juaner.common.data.extension.mybatis;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface VirtualGenerated {
+}
