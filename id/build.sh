510c251efe7f6b7bfb78de27d034c7c129234b36
vicliu624 <vicliu@outlook.com>
2020-10-17 10:17:25 +0800

    使用私有registry

--- id/build.sh
+++ id/build.sh
@@ -2,5 +2,5 @@
 DOCKER_IMAGE='id-service'
 cd `dirname "$0"`
 mvn -Pfat-jar -Dmaven.test.skip=true clean package
-docker build -t registry.cn-chengdu.aliyuncs.com/magicletters/$DOCKER_IMAGE:latest .
-docker push registry.cn-chengdu.aliyuncs.com/magicletters/$DOCKER_IMAGE:latest
+docker build --build-arg HTTP_PROXY=http://vicliu.i234.me:8001 -t vicliu.i234.me:8001/yn-etc/$DOCKER_IMAGE:latest .
+docker push vicliu.i234.me:8001/yn-etc/$DOCKER_IMAGE:latest
