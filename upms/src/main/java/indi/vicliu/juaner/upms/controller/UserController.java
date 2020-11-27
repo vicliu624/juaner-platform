3fff47f672881008c4d19a79dc5058e11b280ab8
xiongguangbo <624202979@qq.com>
2020-11-27 16:17:01 +0800

    增加APP账户注销功能

--- upms/src/main/java/indi/vicliu/juaner/upms/controller/UserController.java
+++ upms/src/main/java/indi/vicliu/juaner/upms/controller/UserController.java
@@ -31,95 +31,98 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/user/fullInfo")
-    public Result getFullUserInfoByUserName(@RequestParam String userName){
-        try{
+    public Result getFullUserInfoByUserName(@RequestParam String userName) {
+        try {
             TblUserInfo userInfo = this.userService.getByUsername(userName);
             return Result.success(userInfo);
-        } catch (Exception e){
-            log.error(" getFullUserInfoByUserName fail ",e);
-            if(e instanceof UserException){
+        } catch (Exception e) {
+            log.error(" getFullUserInfoByUserName fail ", e);
+            if (e instanceof UserException) {
                 return Result.fail(e.getMessage());
             } else {
-                return Result.fail(ErrorType.SYSTEM_ERROR,"查询用户信息时出错");
+                return Result.fail(ErrorType.SYSTEM_ERROR, "查询用户信息时出错");
             }
         }
     }
 
     @GetMapping("/user/getByUsername")
-    public Result getByUsername(@RequestParam("username") String username){
+    public Result getByUsername(@RequestParam("username") String username) {
         try {
             TblUserInfo userInfo = userService.getByUsername(username);
             userInfo.setPassword(null);
             return Result.success(userInfo);
-        }catch (Exception e){
-            log.error(" getByUsername fail ",e);
-            if(e instanceof UserException){
+        } catch (Exception e) {
+            log.error(" getByUsername fail ", e);
+            if (e instanceof UserException) {
                 return Result.fail(e.getMessage());
             } else {
-                return Result.fail(ErrorType.SYSTEM_ERROR,"查询用户信息时出错");
+                return Result.fail(ErrorType.SYSTEM_ERROR, "查询用户信息时出错");
             }
         }
     }
 
     @GetMapping("/user/fullInfo/byPhone")
     public Result getFullUserInfoByPhone(@RequestParam String phone) {
-        try{
+        try {
             TblUserInfo userInfo = this.userService.findByPhone(phone);
             return Result.success(userInfo);
-        } catch (Exception e){
-            log.error(" getFullUserInfoByUserName fail ",e);
-            if(e instanceof UserException){
+        } catch (Exception e) {
+            log.error(" getFullUserInfoByUserName fail ", e);
+            if (e instanceof UserException) {
                 return Result.fail(e.getMessage());
             } else {
-                return Result.fail(ErrorType.SYSTEM_ERROR,"查询用户信息时出错");
+                return Result.fail(ErrorType.SYSTEM_ERROR, "查询用户信息时出错");
             }
         }
     }
 
     /**
      * 创建用户
+     *
      * @param userInfo
      * @return
      */
     @PostMapping("/user/addUser")
-    public Result addUserInfo(@RequestBody @Validated AddUserInfoVO userInfo){
+    public Result addUserInfo(@RequestBody @Validated AddUserInfoVO userInfo) {
         try {
             return Result.success(userService.addUserInfo(userInfo));
         } catch (Exception e) {
-            log.info("创建用户异常：{}",e);
-            if(e instanceof UserException){
+            log.info("创建用户异常：{}", e);
+            if (e instanceof UserException) {
                 return Result.fail(e.getMessage());
-            }else {
-                return Result.fail(ErrorType.SYSTEM_ERROR,"创建用户角色失败");
+            } else {
+                return Result.fail(ErrorType.SYSTEM_ERROR, "创建用户角色失败");
             }
         }
     }
 
     /**
      * 修改用户信息
+     *
      * @param info
      * @return
      */
     @PutMapping("/user/updateUser")
-    public Result updateUserInfo(@RequestBody AddUserInfoVO info){
-        try{
+    public Result updateUserInfo(@RequestBody AddUserInfoVO info) {
+        try {
             userService.updateUserInfo(info);
             return Result.success();
-        } catch (Exception e){
-            log.error("更新用户信息出错",e);
+        } catch (Exception e) {
+            log.error("更新用户信息出错", e);
             return Result.fail(e.getMessage());
         }
     }
 
     /**
      * 根据手机号码查询用户信息
+     *
      * @param phone
      * @return
      */
     @GetMapping("/user/findByUserPhone")
-    public Result findByUserPhone(@RequestParam String phone){
+    public Result findByUserPhone(@RequestParam String phone) {
         TblUserInfo userInfo = userService.findByUserPhone(phone);
-        if(userInfo != null){
+        if (userInfo != null) {
             return Result.success(userInfo);
         } else {
             return Result.success("手机号未绑定过任何用户");
@@ -128,13 +131,14 @@ public class UserController {
 
     /**
      * 根据手机号码查询用户信息
+     *
      * @param userId
      * @return
      */
     @GetMapping("/user/getByUserId")
-    public Result findByUserId(@RequestParam Long userId){
+    public Result findByUserId(@RequestParam Long userId) {
         TblUserInfo userInfo = userService.findByUserId(userId);
-        if(userInfo != null){
+        if (userInfo != null) {
             return Result.success(userInfo);
         } else {
             return Result.success("查询不到用户");
@@ -143,27 +147,29 @@ public class UserController {
 
     /**
      * 创建用户不录入角色
+     *
      * @param userInfo
      * @return
      */
     @PostMapping("/user/createUserInfo")
-    public Result createUserInfo(@RequestBody AddUserInfoVO userInfo){
-        try{
+    public Result createUserInfo(@RequestBody AddUserInfoVO userInfo) {
+        try {
             TblUserInfo userInfo1 = userService.createUserInfo(userInfo);
             return Result.success(userInfo1);
-        } catch (Exception e){
-            log.error("创建用户失败",e);
+        } catch (Exception e) {
+            log.error("创建用户失败", e);
             return Result.fail(e.getMessage());
         }
     }
 
     /**
      * 管理端删除用户
+     *
      * @param ids
      * @return
      */
     @GetMapping("/user/delUserInfo")
-    public Result delUserInfo(@RequestParam("ids") String ids){
+    public Result delUserInfo(@RequestParam("ids") String ids) {
         try {
             userService.delUserInfo(ids);
             return Result.success("删除用户成功");
@@ -174,31 +180,31 @@ public class UserController {
     }
 
     @PutMapping("/user/lock")
-    public Result lockUser(@RequestBody String userName){
-        try{
+    public Result lockUser(@RequestBody String userName) {
+        try {
             int count = userService.lockUserByName(userName);
-            if(count > 0){
+            if (count > 0) {
                 return Result.success("用户被锁定");
             } else {
                 return Result.success("用户已锁定");
             }
-        } catch (Exception e){
-            log.error("锁定用户出错",e);
+        } catch (Exception e) {
+            log.error("锁定用户出错", e);
             return Result.fail(e.getMessage());
         }
     }
 
     @PutMapping("/user/unlock")
-    public Result unlockUser(@RequestBody String userName){
-        try{
+    public Result unlockUser(@RequestBody String userName) {
+        try {
             int count = userService.unlockUserByName(userName);
-            if(count > 0){
+            if (count > 0) {
                 return Result.success("用户解锁成功");
             } else {
                 return Result.success("用户已解锁");
             }
-        } catch (Exception e){
-            log.error("锁定用户出错",e);
+        } catch (Exception e) {
+            log.error("锁定用户出错", e);
             return Result.fail(e.getMessage());
         }
     }
@@ -207,10 +213,10 @@ public class UserController {
     public Result bindPhone(@RequestBody String phone) {
         String userName = UserContextHolder.getInstance().getUsername();
         TblUserInfo userInfo = null;
-        try{
+        try {
             userInfo = this.userService.getByUsername(userName);
-        } catch (UserException e){
-            log.error("绑定手机号出错",e);
+        } catch (UserException e) {
+            log.error("绑定手机号出错", e);
             return Result.fail(e.getMessage());
         }
 
@@ -220,13 +226,24 @@ public class UserController {
     }
 
     @PostMapping("/user/bindWeChat")
-    public Result bindUserFromWeChat(@RequestBody BindWeChatVo weChatVo){
-        try{
+    public Result bindUserFromWeChat(@RequestBody BindWeChatVo weChatVo) {
+        try {
             log.info("request data:{}", JSONObject.toJSONString(weChatVo));
-            return Result.success(this.userService.queryByWeChatUnionId(weChatVo.getUnionid(),weChatVo.getOpenid()));
-        } catch (Exception e){
-            log.error("绑定微信账号出错",e);
+            return Result.success(this.userService.queryByWeChatUnionId(weChatVo.getUnionid(), weChatVo.getOpenid()));
+        } catch (Exception e) {
+            log.error("绑定微信账号出错", e);
             return Result.fail("绑定微信账号出错" + e.getMessage());
+        }
+    }
+
+    @PostMapping("/user/unRegister")
+    public Result unRegister(@RequestBody Long userId) {
+        try {
+            log.info("request data:{}", userId);
+            return userService.unRegister(userId);
+        } catch (Exception e) {
+            log.error("注销账号出错", e);
+            return Result.fail("注销账号出错" + e.getMessage());
         }
     }
 }
