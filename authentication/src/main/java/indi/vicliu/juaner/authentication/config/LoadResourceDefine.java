7690e6ec61e722fc3f51116b48132fee5067c4f0
vicliu624 <vicliu@outlook.com>
2020-12-04 17:58:08 +0800

    测试环境redis配置

--- authentication/src/main/java/indi/vicliu/juaner/authentication/config/LoadResourceDefine.java
+++ authentication/src/main/java/indi/vicliu/juaner/authentication/config/LoadResourceDefine.java
@@ -5,19 +5,19 @@ import indi.vicliu.juaner.authentication.vo.PermissionInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.access.SecurityConfig;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@Component
+@Configuration
 @Slf4j
 public class LoadResourceDefine {
     @Autowired
