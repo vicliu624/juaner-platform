6538fc1b7418a62f81c065ca9e78f907124522db
lwq523698741 <lwq523698741@163.com>
2020-12-23 13:06:01 +0800

    网关拦截非 ynt-etc域名访问

--- gateway/src/main/java/indi/vicliu/juaner/gateway/filter/AccessGatewayFilter.java
+++ gateway/src/main/java/indi/vicliu/juaner/gateway/filter/AccessGatewayFilter.java
@@ -75,8 +75,9 @@ public class AccessGatewayFilter implements GlobalFilter {
             return webSockerFilter.filter(exchange,chain);
         }
 
-        final String referer = request.getHeaders().getFirst(HttpHeaders.REFERER);
+
         try {
+            final String referer = request.getHeaders().getFirst(HttpHeaders.REFERER);
             URI refererUrl = new URI(referer);
             final String refererUrlHost = refererUrl.getHost();
             log.info("当前访问路径为 {},refererHost {}",requestUrl.toString(),refererUrlHost);
@@ -85,8 +86,10 @@ public class AccessGatewayFilter implements GlobalFilter {
                 return unauthorized(exchange);
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("referer错误拦截,当前访问路径为 {},{}",requestUrl.toString(),e.toString());
+            return unauthorized(exchange);
         }
+
 
 
         /*if (StringUtils.isNotEmpty(request.getHeaders().getFirst(WEBSOCKET_PROTOCOL))) {
