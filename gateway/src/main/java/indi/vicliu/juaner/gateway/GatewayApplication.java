77d061a9a04a699a66c577fc26084c207d927abc
lwq523698741 <lwq523698741@163.com>
2020-12-24 15:03:57 +0800

    网关修改test 数据库 配置

--- gateway/src/main/java/indi/vicliu/juaner/gateway/GatewayApplication.java
+++ gateway/src/main/java/indi/vicliu/juaner/gateway/GatewayApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @Auther: liuweikai
@@ -13,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
+@MapperScan("indi.vicliu.juaner.gateway.data.mapper")
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
