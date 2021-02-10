ee2ec823e529e5ccc9baafebdf204831bbe9cc03
vicliu624 <vicliu@outlook.com>
2021-02-10 10:36:25 +0800

    删除不用的引用

--- upms/src/main/java/indi/vicliu/juaner/upms/UpmsApplication.java
+++ upms/src/main/java/indi/vicliu/juaner/upms/UpmsApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @EnableSentinelAspect
